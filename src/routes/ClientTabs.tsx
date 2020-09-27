85bddaf61b774fb36797f25e961fff4e6391376d
caiokronuz <caiogabriel135@gmail.com>
2020-09-27 18:57:59 -0300

    Estilização da tab de navegação pré-finalizada

--- src/routes/ClientTabs.tsx
+++ src/routes/ClientTabs.tsx
@@ -9,10 +9,38 @@ const Tab = createBottomTabNavigator();
 
 function ClientTabs(){
     return(
-        <Tab.Navigator>
-            <Tab.Screen name="Initial" component={InitialPage} />
-            <Tab.Screen name="Schedule" component={SchedulePage} />
-            <Tab.Screen name="Perfil" component={PerfilPage} />
+        <Tab.Navigator
+            tabBarOptions={{
+                style: {
+                    backgroundColor: '#ffdb00',
+                    borderTopColor: '#ffdb00',
+                    height: 30,
+                },
+                inactiveTintColor: '#fff',
+                activeTintColor: '#000'
+            }}
+        >
+            <Tab.Screen 
+                name="Initial" 
+                component={InitialPage} 
+                options={{
+                    tabBarLabel: 'Página Inicial',
+                }}    
+            />
+            <Tab.Screen 
+                name="Schedule" 
+                component={SchedulePage} 
+                options={{
+                    tabBarLabel: 'Agenda'
+                }}    
+            />
+            <Tab.Screen 
+                name="Perfil" 
+                component={PerfilPage}
+                options={{
+                    tabBarLabel: 'Perfil'
+                }} 
+            />
         </Tab.Navigator>
     );
 }
